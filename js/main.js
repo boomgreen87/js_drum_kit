2b2f86eabf363f6b16001b303ec82f354cbc4a3d
Olthoff <s_olthoff2@tss-hr420-dm26.local>
2019-03-04 16:06:20 -0500

    Added animation

--- js/main.js
+++ js/main.js
@@ -14,7 +14,25 @@
 		//Rewind audio on every click and make it play
 		audio.currentTime = 0;
 		audio.play();
+
+		// Grab the div and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+		key.classList.add("playing");
 	}
+
+	function removePlayingClass(event) {
+		// debugger;
+		if (event.propertyName !== "transform") {
+			return;
+		} else {
+			//Remove the playing class here fom the active div
+			console.log("Transition is done");
+			event.target.classList.remove("playing");
+		}
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 
 	window.addEventListener("keydown", playSound);
 })();
